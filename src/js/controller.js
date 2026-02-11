91cce66c03d93061fdc1ab5f581874fc4997dfb4
Dunay Gudratli <dunay.gudretli@azercell.com>
2026-02-11 18:44:40 +0400

    32: listen for load and hashchange events

--- src/js/controller.js
+++ src/js/controller.js
@@ -31,10 +31,13 @@ const renderSpinner = function (parent) {
 
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+    if (!id) return;
+
     // loading recipe
     renderSpinner(recipeContainer);
     const res = await fetch(
-      'https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886'
+      `https://forkify-api.jonas.io/api/v2/recipes/${id}`
     );
     const data = await res.json();
 
@@ -160,4 +163,6 @@ const showRecipe = async function () {
   }
 };
 
-showRecipe();
+['hashchange', 'load'].forEach(event =>
+  window.addEventListener(event, showRecipe)
+);
