08903b631086533e63b54a870c198561fb9fd7dc
Dunay Gudratli <dunay.gudretli@azercell.com>
2026-02-12 18:30:57 +0400

    311: implement pagination

--- src/js/controller.js
+++ src/js/controller.js
@@ -4,10 +4,11 @@ import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView';
 import resultsView from './views/resultsView';
+import paginationView from './views/paginationView';
 
-if (module.hot){
-  module.hot.accept();
-}
+// if (module.hot){
+//   module.hot.accept();
+// }
 
 const controlRecipes = async function() {
   try {
@@ -34,17 +35,25 @@ const controlSearchResults = async function() {
     if (!query) return;
 
     await model.loadSearchResults(query);
-    resultsView.render(model.state.search.results)
+    resultsView.render(model.getSearchResultsPage());
 
+    paginationView.render(model.state.search);
   } catch (err) {
     console.log(err);
     recipeView.renderError();
   }
 };
 
+const controlPagination = function(page) {
+  resultsView.render(model.getSearchResultsPage(page));
+
+  paginationView.render(model.state.search);
+};
+
 const init = function() {
   recipeView.addHandlerRender(controlRecipes);
   searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
 };
 
 init();
--- src/js/model.js
+++ src/js/model.js
@@ -1,11 +1,13 @@
-import { API_URL } from './config';
+import { API_URL, RESULT_PER_PAGE } from './config';
 import { getJSON } from './helpers';
 
 export const state = {
   recipe: {},
   search: {
     query: '',
-    results: []
+    results: [],
+    page: 1,
+    resultsPerPage: RESULT_PER_PAGE,
   }
 };
 
@@ -48,3 +50,12 @@ export const loadSearchResults = async function(query) {
     throw err;
   }
 };
+
+export const getSearchResultsPage = function(page = state.search.page){
+  state.search.page = page;
+
+  const start = (page - 1) * state.search.resultsPerPage;
+  const end = page * state.search.resultsPerPage;
+
+  return state.search.results.slice(start, end);
+}
